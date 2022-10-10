4afa74ef5973208b881a25714ec72723700565e8
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-10-10 15:42:05 +0700

    learn HOC through example template for app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoadingComponent from "./components/GlobalSetting/LoadingGlobalComponents/LoadingComponent";
-import Header from "./components/Home/Header/Header";
+// import Header from "./components/Home/Header/Header";
 import Modal from "./HOC/Modal/Modal";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
@@ -13,27 +13,49 @@ import Profile from "./pages/Profile/Profile";
 import Todolist from "./pages/Todolist/Todolist";
 import TodolistRedux from "./pages/Todolist/TodolistRedux";
 import TodolistRFC from "./pages/Todolist/TodolistRFC";
-import HomeSaga from "./pages/TodolistSaga/HomeSaga"
+import HomeSaga from "./pages/TodolistSaga/HomeSaga";
+import { HomeTemplate } from "./templates/HomeTemplate/HomeTemplate";
 function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <Modal/>
-    <LoadingComponent/>
+      <Modal />
+      <LoadingComponent />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/login" component={Login}/>
-        <Route exact path="/profile" component={Profile}/>
-        <Route exact path="/detail/:id" component={Detail}/>
-        <Route exact path="/todolistrcc" component={Todolist}/>
-        <Route exact path="/todolistrfc" component={TodolistRFC}/>
+
+        {/* <Route
+          exact
+          path="/home"
+          render={(propsRoute) => {
+            return (
+              <div>
+                <Header />
+                <Home />
+              </div>
+            );
+          }}
+        /> */}
+
+        <HomeTemplate exact path="/home" Component={Home}/>
+
+        <Route exact path="/contact" render={(propsRoute)=>{
+          return(
+            <div style={{background: "#FFF"}}>
+              <Contact/>
+            </div>
+          );
+        }} />
+
+        <HomeTemplate exact path="/about" Component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/detail/:id" component={Detail} />
+        <Route exact path="/todolistrcc" component={Todolist} />
+        <Route exact path="/todolistrfc" component={TodolistRFC} />
         <Route exact path="/todolistredux" component={TodolistRedux} />
         <Route exact path="/todolistsaga" component={HomeSaga} />
         <Route exact path="/demoHOC" component={DemoHOCModal} />
-        <Route path="*" component={PageNotFound}/>
+        <Route path="*" component={PageNotFound} />
       </Switch>
     </BrowserRouter>
   );
