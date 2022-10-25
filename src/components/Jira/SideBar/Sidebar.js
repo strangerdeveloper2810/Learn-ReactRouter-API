c546879e0065251f4ed89823c1f0c75151ea1a54
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-10-25 14:16:36 +0700

    fix ui sidebar

--- src/components/Jira/SideBar/Sidebar.js
+++ src/components/Jira/SideBar/Sidebar.js
@@ -1,33 +1,43 @@
-import React from "react";
-
+import {
+  BarsOutlined,
+  PlusOutlined,
+  SearchOutlined  
+} from "@ant-design/icons/lib/icons";
+import { Layout, Menu } from "antd";
+import React, { useState } from "react";
+const { Sider } = Layout;
 export default function Sidebar(props) {
+  const [state, setState] = useState({
+    collapsed: false,
+  });
+  const toggle = () => {
+    setState({
+      collapsed: !state.collapsed,
+    });
+  };
   return (
-    <div className="sideBar">
-      <div className="sideBar-top">
-        <div className="sideBar-icon">
-          <i className="fab fa-jira" />
+    <div>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={state.collapsed}
+        style={{ height: "100%" }}
+      >
+        <div className="text-end pe-2" onClick={toggle}>
+          <BarsOutlined
+            style={{ cursor: "pointer", color: "#fff", fontSize: 25 }}
+          />
         </div>
-        <div
-          className="sideBar-icon"
-          data-toggle="modal"
-          data-target="#searchModal"
-          style={{ cursor: "pointer" }}
-        >
-          <i className="fa fa-search" />
-          <span className="title"> SEARCH ISSUES</span>
-          
-        </div>
-        <div className="sideBar-icon">
-          <i className="fa fa-plus" />
-          <span className="title"> CREATE ISSUES</span>
-        </div>
-      </div>
-      <div className="sideBar-bottom">
-        <div className="sideBar-icon">
-          <i className="fa fa-question-circle" />
-          <span className="title"> ABOUT</span>
-        </div>
-      </div>
+
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1" icon={<PlusOutlined style={{ fontSize: 20 }} />}>
+            <span className="mb-2">Create issue</span>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<SearchOutlined style={{ fontSize: 20 }} />}>
+            Search
+          </Menu.Item>
+        </Menu>
+      </Sider>
     </div>
   );
 }
