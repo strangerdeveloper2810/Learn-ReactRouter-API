7deb3d435c38c254bd45d6b6a4ef4f25938f7be6
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-10-26 14:08:48 +0700

    Fix UI Sidebar and Menu, Create Jira Modal

--- src/components/Jira/SideBar/Sidebar.js
+++ src/components/Jira/SideBar/Sidebar.js
@@ -1,8 +1,9 @@
 import {
   BarsOutlined,
   PlusOutlined,
-  SearchOutlined  
+  SearchOutlined,
 } from "@ant-design/icons/lib/icons";
+
 import { Layout, Menu } from "antd";
 import React, { useState } from "react";
 const { Sider } = Layout;
@@ -16,28 +17,29 @@ export default function Sidebar(props) {
     });
   };
   return (
-    <div>
-      <Sider
-        trigger={null}
-        collapsible
-        collapsed={state.collapsed}
-        style={{ height: "100%" }}
-      >
-        <div className="text-end pe-2" onClick={toggle}>
-          <BarsOutlined
-            style={{ cursor: "pointer", color: "#fff", fontSize: 25 }}
-          />
-        </div>
-
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<PlusOutlined style={{ fontSize: 20 }} />}>
-            <span className="mb-2">Create issue</span>
-          </Menu.Item>
-          <Menu.Item key="2" icon={<SearchOutlined style={{ fontSize: 20 }} />}>
-            Search
-          </Menu.Item>
-        </Menu>
-      </Sider>
-    </div>
+    <Sider trigger={null}  collapsible collapsed={state.collapsed} style={{ height: "100%" }}>
+      <div className="text-end pe-2" onClick={toggle}>
+        <BarsOutlined
+          style={{ cursor: "pointer", color: "#fff", fontSize: 25 }}
+        />
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["1"]}
+        items={[
+          {
+            key: "1",
+            icon: <PlusOutlined style={{ fontSize: 20 }} />,
+            label: "Create issue",
+          },
+          {
+            key: "2",
+            icon: <SearchOutlined style={{ fontSize: 20 }} />,
+            label: "Search",
+          },
+        ]}
+      />
+    </Sider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,17 +7,15 @@ import store from "./redux/configStore";
 import { Provider } from "react-redux";
 import "antd/dist/antd.min.css";
 import { Router } from "react-router-dom";
-import history from "./util/history"
+import history from "./util/history";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router history={history}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
-  </React.StrictMode>
+  <Router history={history}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
